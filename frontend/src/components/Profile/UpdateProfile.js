8bae9219d5c3465198de14f90379c896ec2683f8
Dipto <tdipto@yahoo.com>
2024-01-22 01:09:16 +0600

    update profile

--- frontend/src/components/Profile/UpdateProfile.js
+++ frontend/src/components/Profile/UpdateProfile.js
@@ -1,86 +1,143 @@
+import axios from "axios";
 import React, { useState } from "react";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
-import { api } from "../../utils/api";
-import axiosInstance from "../../utils/axios";
 
-const CreateProfile = () => {
-  const navigate = useNavigate();
+import { updateProfile } from "../../features/profile/profileSlice";
+
+const UpdateProfile = () => {
   const { user } = useSelector((state) => state.auth);
-
   const address = user?.profile?.address;
   const phone = user?.profile?.phone;
 
+  const navigate = useNavigate();
+  const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+
+  const dispatch = useDispatch();
+  const handleImageUpload = async (e) => {
+    setLoading(true);
+    const files = e.target.files;
+
+    const data = new FormData();
+    for (let i = 0; i < files.length; i++) {
+      data.append("file", files[i]);
+      data.append("upload_preset", "software");
+      data.append("tags", "multiple_images");
+      try {
+        const res = await axios.post(
+          `https://api.cloudinary.com/v1_1/du55ossud/image/upload`,
+          data
+        );
+
+        setImages((prevImages) => [
+          ...prevImages,
+          prevImages.push(res.data.secure_url),
+        ]);
+      } catch (err) {
+        console.error("Error uploading image: ", err);
+      }
+    }
+    setLoading(false);
+  };
+
   const [formData, setFormData] = useState({
     address: address,
+    images: images,
     phone: phone,
   });
 
   const handleInputChange = (e) => {
-    const { name, value } = e.target;
+    const { name, value, type } = e.target;
+    const newValue = type === "number" ? parseInt(value) : value;
 
     setFormData({
       ...formData,
-      [name]: value,
+      [name]: newValue,
     });
   };
 
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    console.log(formData);
-
     try {
-      const response = await axiosInstance.put(`${api}/profile/edit`, {
-        ...formData,
-      });
-
-      if (response.data) {
-        navigate("/profile");
-
-        setFormData({
-          address: "",
-
-          phone: "",
-        });
-      }
+      dispatch(updateProfile(formData));
+      navigate("/profile");
+      // const response = await axiosInstance.put(`${api}/profile/edit`, {
+      //   ...formData,
+      // });
+      // if (response.data) {
+      //   navigate("/");
+      //   setFormData({
+      //     address: "",
+      //     images: [],
+      //     phone: "",
+      //   });
+      // }
     } catch (error) {
       console.error("Error posting data:", error);
     }
   };
 
   return (
-    <div className="pt-28">
-      {/* {console.log(user)} */}
+    <div className="w-[60%] h-screen mx-auto flex flex-row justify-center items-center">
       <form
         onSubmit={handleSubmit}
-        className=" w-[90vh]  mx-auto my-5 p-4 bg-gray-100 rounded-lg"
+        className=" w-[90vh] h-auto  mx-auto my-5 p-4 bg-gray-100 rounded-lg"
       >
+        <label>
+          Address:
+          <input
+            type="text"
+            name="address"
+            value={formData.address}
+            onChange={handleInputChange}
+            placeholder="Address"
+            className="w-full border rounded-md p-2 mb-4"
+          />
+        </label>
+        <label>
+          Phone:
+          <input
+            type="text"
+            name="phone"
+            value={formData.phone}
+            onChange={handleInputChange}
+            placeholder="Phone"
+            className="w-full border rounded-md p-2 mb-4"
+          />
+        </label>
+        <h2> Upload profile photo:</h2>
         <input
-          type="text"
-          name="address"
-          value={formData.address}
-          onChange={handleInputChange}
-          placeholder="Address"
-          className="w-full border rounded-md p-2 mb-4"
+          type="file"
+          onChange={handleImageUpload}
+          multiple
+          className="mb-4"
         />
-        <input
-          type="text"
-          name="phone"
-          value={formData.phone}
-          onChange={handleInputChange}
-          placeholder="Phone"
-          className="w-full border rounded-md p-2 mb-4"
-        />
+
+        {loading && <p className="text-red-500">Uploading...</p>}
+
+        {formData.images.length > 0 && (
+          <div className="mb-4 flex flex-row">
+            {formData.images.map((photo, index) => (
+              <img
+                key={index}
+                src={photo}
+                alt={`Photo_no ${index}`}
+                className="w-20 h-20 object-cover mr-2 mb-2"
+              />
+            ))}
+          </div>
+        )}
         <button
           type="submit"
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
         >
-          Update
+          Update Profile
         </button>
       </form>
     </div>
   );
 };
 
-export default CreateProfile;
+export default UpdateProfile;

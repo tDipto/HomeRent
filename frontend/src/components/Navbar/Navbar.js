44f3adb17346c78b0e4431c98585c9b79bac6596
Dipto <tdipto@yahoo.com>
2024-01-25 17:25:56 +0600

    profile fixed

--- frontend/src/components/Navbar/Navbar.js
+++ frontend/src/components/Navbar/Navbar.js
@@ -3,14 +3,15 @@ import { useDispatch, useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { fetchLoggedOutUser } from "../../features/auth/authSlice";
 import { fetchUserProfile } from "../../features/profile/profileSlice";
-import logo from '../Navbar/logo.png'
+import logo from "../Navbar/logo.png";
 
 // console.log(logo);
 
 const Navbar = () => {
   const { isLoggedIn, user, role } = useSelector((state) => state.auth);
   // console.log(isLoggedIn, role);
-  const image = user?.profile?.images;
+  const image = user?.profile?.images[0];
+
   const profile = user?.profile | {};
   const dispatch = useDispatch();
 
@@ -25,19 +26,24 @@ const Navbar = () => {
     dispatch(fetchUserProfile());
     navigate("/profile");
   };
-
-  
+  const name = user?.name;
   return (
     <div className="navbar bg-custom text-black fixed top-0 w-full">
       <div className="flex-1">
-          <Link to="/" className="flex flex-row">
-              <div>
-                <img src={logo} alt="Logo" style={{ width: '80px', height: '80px' }}/>
-              </div>
-              <div className="text-xl mt-6">
-                <span>RentSpot : <small>Home Near CUET</small></span>
-              </div>
-          </Link>  
+        <Link to="/" className="flex flex-row">
+          <div>
+            <img
+              src={logo}
+              alt="Logo"
+              style={{ width: "80px", height: "80px" }}
+            />
+          </div>
+          <div className="text-xl mt-6">
+            <span>
+              RentSpot : <small>Home Near CUET</small>
+            </span>
+          </div>
+        </Link>
         {/* <Link to="/" className="btn btn-ghost text-xl bg-gray">
           RentSpot : <small>Home Near CUET</small>
         </Link> */}
@@ -57,50 +63,54 @@ const Navbar = () => {
             </Link>
           )}
           {!isLoggedIn && (
-            <Link to="/login" className="btn btn-ghost bg-blue-400">
-              Login
-            </Link>
+            <>
+              <Link to="/login" className="btn btn-ghost bg-blue-400">
+                Login
+              </Link>
+              <Link to="/register" className="btn mx-3 btn-ghost bg-blue-400">
+                Register
+              </Link>
+            </>
           )}
-          {!isLoggedIn && (
-            <Link to="/register" className="btn mx-3 btn-ghost bg-blue-400">
-              Register
-            </Link>
-          )}
+          {/* {!isLoggedIn && (
+            
+          )} */}
         </div>
         {isLoggedIn && (
-          <div className="dropdown dropdown-end">
-            <div
-              tabIndex={0}
-              role="button"
-              className="btn btn-ghost btn-circle avatar"
-            >
-              <div className="w-10 rounded-full">
-                <img
-                  alt="Tailwind CSS Navbar component"
-                  src={
-                    profile !== null
-                      ? image
-                      : "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
-                  }
-                />
+          <>
+            <button onClick={handleProfile} type="button">
+              {name}
+            </button>
+            <div className="dropdown dropdown-end">
+              <div
+                tabIndex={0}
+                role="button"
+                className="btn btn-ghost btn-circle avatar"
+              >
+                <div className="w-10 rounded-full">
+                  <img
+                    alt="Tailwind CSS Navbar component"
+                    src={
+                      profile !== null
+                        ? image
+                        : "https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg"
+                    }
+                  />
+                </div>
               </div>
+
+              <ul
+                tabIndex={0}
+                className="mt-3 z-[1] py-3 shadow menu menu-sm dropdown-content bg-white rounded-box w-60"
+              >
+                <li className="p-1">
+                  <button onClick={handleLogout} type="button">
+                    Logout
+                  </button>
+                </li>
+              </ul>
             </div>
-            <ul
-              tabIndex={0}
-              className="mt-3 z-[1] py-3 shadow menu menu-sm dropdown-content bg-white rounded-box w-60"
-            >
-              <li className="p-1">
-                <button onClick={handleProfile} type="button">
-                  Profile
-                </button>
-              </li>
-              <li className="p-1">
-                <button onClick={handleLogout} type="button">
-                  Logout
-                </button>
-              </li>
-            </ul>
-          </div>
+          </>
         )}
       </div>
     </div>
